5f099b745dc365a2edd19d8e517493fcb635189a
Henrique Veronez <v3ronez.dev@gmail.com>
2023-02-20 19:38:47 -0300

    wip

--- 6-ponteiro/ponteiro.go
+++ 6-ponteiro/ponteiro.go
@@ -2,11 +2,11 @@ package main
 
 func main() {
 	//ponteiros sao referencias de memorias e não gurdam o valor em si da variavel mas sim a referencia dela na memoria
-
+	// * indica que é um ponteiro e & voce referencia outra variavel
 	var num1 int = 12
-	var ponteiro_num1 *int = &num1 // * indica que é um ponteiro e & voce referencia outra variavel
+	var ponteiro_num1 *int = &num1 // ponteiro_num1 tem o valor da referencia na memoria de num1
 	println(num1, ponteiro_num1)
 	num1 = 30
-	println(num1, *ponteiro_num1) // * para pegar o valor do ponteiro em vez da referencia (desreferenciar)
+	println(num1, *ponteiro_num1) // * para pegar o valor do ponteiro em vez da referencia (desreferenciar) 
 
 }
