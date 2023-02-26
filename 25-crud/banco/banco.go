34136560d8c51324350648dbd511c2946adf59ec
Henrique Veronez <v3ronez.dev@gmail.com>
2023-02-25 23:21:58 -0300

    create crud

---
+++ 25-crud/banco/banco.go
@@ -0,0 +1,20 @@
+package banco
+
+import (
+	"database/sql"
+
+	_ "github.com/lib/pq" // import implicido que o sql do golang precisa
+)
+
+func Connectar() (*sql.DB, error) {
+	strConnect := "user=veronez password=261602317 dbname=golang_studies sslmode=disable"
+	db, err := sql.Open("postgres", strConnect)
+
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
+}
