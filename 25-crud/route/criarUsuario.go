133ea252b01f9cf80fe2ae9cf883a1a0f58fabed
Henrique Veronez <v3ronez.dev@gmail.com>
2023-02-26 11:29:12 -0300

    insert user

--- 25-crud/route/criarUsuario.go
+++ 25-crud/route/criarUsuario.go
@@ -6,8 +6,6 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-
-	"github.com/lib/pq"
 )
 
 type usuarioType struct {
@@ -33,28 +31,20 @@ func CriarUsuario(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	defer db.Close()
-	beginTrans, err := db.Begin()
-	if err != nil {
-		w.Write([]byte("Erro ao iniciar transicao"))
-		// "Erro ao criar o statement."
-		return
-	}
 
-	statement, err := beginTrans.Prepare(pq.CopyIn("usuarios", "id", "nome", "email"))
+	statement, err := db.Prepare("INSERT INTO usuarios(nome, email) VALUES ($1, $2) RETURNING id")
 	if err != nil {
-		w.Write([]byte(err.Error()))
-		// "Erro ao criar o statement."
+		w.Write([]byte("Erro ao criar o statement."))
 		return
 	}
-	beginTrans.Commit()
 	defer statement.Close()
 
-	// inserir, err := statement.Exec(usuario.Nome, usuario.Email)
-	// if err != nil {
-	// 	w.Write([]byte("Erro ao inserir o usuario."))
-	// 	return
-	// }
+	inserir, err := statement.Exec(usuario.Nome, usuario.Email)
+	if err != nil {
+		w.Write([]byte("Erro ao inserir o usuario."))
+		return
+	}
 
 	w.WriteHeader(http.StatusCreated) //201
-	w.Write([]byte(fmt.Sprintln("Usuario cadastrado")))
+	w.Write([]byte(fmt.Sprintf("Usuario inserido com sucesso: %d", inserir)))
 }
